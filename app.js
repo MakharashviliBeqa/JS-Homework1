e47015bc117daa8b7206e0900157082a32e27093
MakharashviliBeqa <201153206+MakharashviliBeqa@users.noreply.github.com>
2025-06-15 20:13:29 +0400

    added js and index.html

---
+++ app.js
@@ -0,0 +1,6 @@
+let name="Beqa"
+let dayoftheweek="Friday"
+let favoriteactivity = "Gaming"
+
+const info =`Hi, my name is ${name}! Today is ${dayoftheweek}. a perfect day for ${favoriteactivity}.`
+console.log(info)
